0513c3316f596c487b8073ab48fe9f8837be0e97
Judith van Leersum <judithvleersum@gmail.com>
2022-08-04 15:59:26 +0200

    Added multiple tests for different states of the component

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
@@ -8,9 +9,28 @@ describe("Greeting component", () => {
     expect(helloWorldElement).toBeInTheDocument();
   });
 
-  test("renders It\'s good to see you! as text if button is not clicked", () => {
+  test("renders It\'s good to see you! as text if button is NOT clicked", () => {
     render(<Greeting />);
     const goodToSeeYouElement = screen.getByText("It's good to see you!");
     expect(goodToSeeYouElement).toBeInTheDocument();
   })
+
+  test("renders Changed! as text if button was clicked", () => {
+    render(<Greeting />);
+    //Act 
+    const buttonElement = screen.getByRole('button')
+    userEvent.click(buttonElement)
+    const changedElement = screen.getByText("Changed!");
+    expect(changedElement).toBeInTheDocument();
+  })
+
+  test("does not render good to see you if button was clicked", () => {
+    render(<Greeting />);
+    //Act 
+    const buttonElement = screen.getByRole('button')
+    userEvent.click(buttonElement)
+    const goodToSeeYouElement = screen.queryByText("It's good to see you!");
+    expect(goodToSeeYouElement).not.toBeInTheDocument();
+  })
+
 });
